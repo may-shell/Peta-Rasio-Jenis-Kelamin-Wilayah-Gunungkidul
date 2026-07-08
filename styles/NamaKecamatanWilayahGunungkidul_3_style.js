5c41a1d431cb0f1747009d77036f6b0e0a355f2f
may-shell <2502010023@taruna.stpn.ac.id>
2026-07-08 13:48:34 +0700

    Add files via upload

---
+++ styles/NamaKecamatanWilayahGunungkidul_3_style.js
@@ -0,0 +1,33 @@
+var size = 0;
+var placement = 'point';
+
+var style_NamaKecamatanWilayahGunungkidul_3 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("''");
+    var labelFont = "13.0px \'Times New Roman\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if (feature.get("WADMKC") !== null) {
+        labelText = String(feature.get("WADMKC"));
+    }
+    
+    var style = [ new ol.style.Style({
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor, bufferWidth,
+                              textAlign, offsetX, offsetY, overflow, repeat)
+    })];;
+
+    return style;
+};
